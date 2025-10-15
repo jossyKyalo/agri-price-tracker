fae8d54bb2b0034f6f760bdfc99a88e2f1ff874b
Josephine Kyalo <145260844+jossyKyalo@users.noreply.github.com>
2025-10-16 01:04:35 +0300

    progress..

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,5 +1,7 @@
 import pg from 'pg';
 import { logger } from '../utils/logger'
+import dotenv from "dotenv";
+dotenv.config();
 
 const { Pool } = pg;
 
@@ -9,7 +11,7 @@ const dbConfig = {
   port: parseInt(process.env.DB_PORT || '5432'),
   database: process.env.DB_NAME || 'agri_price_tracker',
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
+  password: process.env.DB_PASSWORD ,
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
   connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
